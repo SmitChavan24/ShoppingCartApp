a880dc6cc0c683c6d24672e7831351a8bec27cec
smit <smitchavan@Z.local>
2024-02-09 13:49:06 +0530

    added code for the creen

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -2,20 +2,21 @@ import {StyleSheet, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-const DetailScreen = () => {
+const DetailScreen = props => {
+  const params = props.route.params.item.id;
+  console.log(params, typeof params);
   const [Data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     FetchUniqueProducts();
   }, []);
   const FetchUniqueProducts = async () => {
-    let params = 1;
     let response = await axios.get(`https://dummyjson.com/products/${params}`);
-    setData(response.data.products);
-    console.log(response.data);
+    setData(response.data);
   };
   return (
     <View>
+      {console.log(Data)}
       <Text>DetailScreen</Text>
     </View>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,13 +6,19 @@ import {
   StatusBar,
   Dimensions,
   TextInput,
+  ScrollView,
+  FlatList,
+  Pressable,
+  Image,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import {useNavigation} from '@react-navigation/native';
 import LinearGradient from 'react-native-linear-gradient';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import shadowProp from '../utils/shadowProp';
 
-const {width} = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 const HomeScreen = () => {
   const [Data, setData] = useState([]);
@@ -25,11 +31,12 @@ const HomeScreen = () => {
   const FetchProducts = async () => {
     let response = await axios.get('https://dummyjson.com/products');
 
-    setData(response.data);
-    console.log(Data, '<=====products');
+    setData(response.data.products);
+    // console.log(Data, '<=====products');
   };
+  // console.log(Data);
   return (
-    <View>
+    <View style={{backgroundColor: 'white', flex: 1}}>
       <StatusBar
         backgroundColor="#ffb3ff"
         barStyle={Platform.OS === 'android' ? 'dark-content' : 'light-content'}
@@ -40,11 +47,15 @@ const HomeScreen = () => {
         useAngle={true}
         angle={180}
         colors={['#ffb3ff', '#f2f2f2']}
-        style={{
-          width: '100%',
-          height: '30%',
-          alignItems: 'center',
-        }}>
+        style={[
+          {
+            width: '100%',
+            height: '15%',
+            alignItems: 'center',
+            backgroundColor: 'white',
+          },
+          shadowProp(15, 'black'),
+        ]}>
         <Text
           style={{
             fontSize: 20,
@@ -54,29 +65,155 @@ const HomeScreen = () => {
           }}>
           Welcome to Shopping Cart
         </Text>
-        <TextInput
-          placeholder="Search"
+        <View
           style={{
-            width: '50%',
-            height: 30,
-            marginLeft: '2.5%',
-            fontSize: 12,
-            borderColor: 'black',
-            borderWidth: 1,
-            textAlign: 'center',
-            borderRadius: 5,
-          }}></TextInput>
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginTop: '5%',
+            alignSelf: 'flex-end',
+          }}>
+          <AntDesign name="search1" size={25} color="black" />
+          <TextInput
+            placeholder="Search"
+            style={{
+              width: '70%',
+              height: 30,
+              fontSize: 15,
+              backgroundColor: '#e6e6e6',
+              borderColor: 'black',
+              borderWidth: 1,
+              paddingVertical: 0.1,
+              textAlignVertical: 'center',
+              fontWeight: '800',
+              paddingLeft: '3%',
+              marginLeft: '4%',
+              alignSelf: 'flex-end',
+              marginRight: '5%',
+              borderRadius: 3,
+            }}></TextInput>
+        </View>
       </LinearGradient>
 
-      {/* <Button
-        title="press"
-        onPress={() => navigation.navigate('details')}
-        
-        ></Button> */}
+      <FlatList
+        data={Data}
+        keyExtractor={id => id.id}
+        showsVerticalScrollIndicator={false}
+        style={{
+          // marginTop: 10,
+          alignSelf: 'center',
+          paddingVertical: '5%',
+        }}
+        renderItem={({item, index}) => (
+          <View style={{marginBottom: '5%', marginTop: 10}}>
+            <Pressable
+              style={[
+                {
+                  height: (height * 20) / 100,
+                  width: (width * 90) / 100,
+                  borderRadius: 5,
+                  borderColor: '#f2f2f2',
+                  borderWidth: 1,
+                  backgroundColor: 'white',
+                  alignItems: 'center',
+                  flexDirection: 'row',
+                },
+                shadowProp(1),
+              ]}
+              key={index}
+              onPress={() => navigation.navigate('details', {item})}>
+              <Image
+                source={{
+                  uri: item.thumbnail,
+                }}
+                style={{
+                  height: (height * 17) / 100,
+                  flex: 1,
+                  marginLeft: '3%',
+                  borderRadius: 2,
+                  borderColor: 'grey',
+                  borderWidth: 0.5,
+                }}
+              />
+              <View
+                style={{
+                  height: (height * 17) / 100,
+                  flex: 2,
+                  marginLeft: '5%',
+                  marginRight: '3%',
+                  marginTop: '5%',
+                }}>
+                <Text
+                  style={{
+                    fontWeight: '700',
+                    letterSpacing: 0.3,
+                    fontSize: 16,
+                    color: 'black',
+                  }}>
+                  {item.title}
+                </Text>
+
+                <View style={{marginTop: '2%'}}>
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={styles.description}>{'Brand '}</Text>
+                    <Text style={styles.dataDesc}>{item.brand}</Text>
+                  </View>
+
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={styles.description}>{'Price '}</Text>
+                    <Text style={styles.dataDesc}>{`${item.price} INR`}</Text>
+                  </View>
+
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={styles.description}>{'Ratings '}</Text>
+                    <Text style={styles.dataDesc}>{item.rating}</Text>
+                  </View>
+
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={styles.description}>{'Discount '}</Text>
+                    <Text
+                      style={
+                        styles.dataDesc
+                      }>{`${item.discountPercentage} %`}</Text>
+                  </View>
+                </View>
+              </View>
+            </Pressable>
+          </View>
+        )}
+      />
+
+      <LinearGradient
+        start={{x: 1, y: 0}}
+        end={{x: 0, y: 0}}
+        useAngle={true}
+        angle={360}
+        colors={['#ffb3ff', '#f2f2f2']}
+        style={[
+          {
+            alignSelf: 'baseline',
+            backgroundColor: 'white',
+            width: '100%',
+            height: '5%',
+          },
+          shadowProp(15, 'black'),
+        ]}></LinearGradient>
     </View>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  line: {
+    width: '90%',
+    height: 0.2,
+    backgroundColor: 'grey',
+  },
+  description: {
+    fontSize: 10,
+    width: '25%',
+    fontFamily: 'PlusJakartaSans-SemiBoldItalic',
+    marginVertical: '1.5%',
+  },
+  dataDesc: {fontSize: 14, color: '#000033', width: '70%'},
+});
